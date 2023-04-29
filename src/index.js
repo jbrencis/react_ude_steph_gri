c33d254904da99d0a7d4015cee80b2983d4471a9
jevgenijs brencis <jevgenijs.brencis@gmail.com>
2023-04-29 13:46:15 +0300

    useReducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { NavigationProvider } from './context/navigation';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(
-  <NavigationProvider>
-    <App />
-  </NavigationProvider>
-);
+root.render(<App />);
