3acd17e587319864f0cd7aaf13b10ba365c454a0
jevgenijs brencis <jevgenijs.brencis@gmail.com>
2023-05-07 13:49:13 +0300

    slices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import "bulma/css/bulma.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
+
+import { store } from "./store";
+import "./styles.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
